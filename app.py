9ed29cf5f1f4a7f7a47d7785b2a710bd8fdc622d
Klaus-012 <klaus0257@gmail.com>
2022-06-26 10:34:51 +0300

    msg

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, redirect, url_for, request
+import cv2
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['email'] == 'user@mail.com' or request.form['password'] == 'user':
+            return redirect(url_for('voter_login'))
+        elif request.form['email'] == 'admin@mail.com' or request.form['password'] == 'admin':
+            return redirect(url_for('register_voter'))
+        else:
+            error = 'Invalid Credentials ! Please try again'
+    return render_template("index.html", error=error)
+
+
+@app.route("/register_voter")
+def register_voter():
+    return render_template("register_voter.html")
+
+
+@app.route("/register_candidate")
+def register_candidate():
+    return render_template("register_candidate.html")
+
+
+@app.route("/vote")
+def vote():
+    return render_template("vote.html")
+
+
+@app.route("/voter_login")
+def voter_login():
+    return render_template("voter_login.html")
+
+
+@app.route("/vote_tallying")
+def vote_tallying():
+    return render_template("vote_tallying.html")
+
+
+def facial_recognition():
+    pass
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)
